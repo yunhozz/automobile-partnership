64342eb4f213421856240de542d3abd0f0647c2a
yunhozz <qkrdbsgh1121@naver.com>
2022-10-05 12:12:37 +0900

    initial commit

---
+++ src/main/java/com/automobilepartnership/AutomobilePartnershipApplication.java
@@ -0,0 +1,13 @@
+package com.automobilepartnership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomobilePartnershipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomobilePartnershipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/automobilepartnership/AutomobilePartnershipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.automobilepartnership;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomobilePartnershipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
