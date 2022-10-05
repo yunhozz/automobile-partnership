25f9afe77bf307c3a977902c8d044124c534f555
yunhozz <qkrdbsgh1121@naver.com>
2022-10-05 19:52:25 +0900

    Feat: security 관련 파일 추가

---
+++ src/main/java/com/automobilepartnership/config/SecurityConfig.java
@@ -0,0 +1,83 @@
+package com.automobilepartnership.config;
+
+import com.automobilepartnership.security.jwt.JwtAccessDeniedHandler;
+import com.automobilepartnership.security.jwt.JwtAuthenticationEntryPoint;
+import com.automobilepartnership.security.jwt.JwtFilter;
+import com.automobilepartnership.security.jwt.JwtProvider;
+import com.automobilepartnership.security.oauth.OAuth2UserServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtProvider jwtProvider;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final OAuth2UserServiceImpl oAuth2UserService;
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // TODO: OAuth2 추가
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception {
+        JwtFilter jwtFilter = new JwtFilter(jwtProvider);
+
+        security
+                .cors().disable()
+                .csrf().disable()
+                .authorizeRequests()
+                .anyRequest().permitAll();
+
+        security
+                // header
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+
+                .and()
+
+                // session
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+
+                // jwt
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling()
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+
+                .and()
+
+                // oauth2
+                .oauth2Login()
+                .authorizationEndpoint()
+                .baseUri("/oauth2/authorization")
+
+                .and()
+
+                .redirectionEndpoint()
+                .baseUri("/*/oauth2/code/*")
+
+                .and()
+
+                .userInfoEndpoint()
+                .userService(oAuth2UserService);
+
+        return security.build();
+    }
+}
---
+++ src/main/java/com/automobilepartnership/security/UserPrincipal.java
@@ -0,0 +1,85 @@
+package com.automobilepartnership.security;
+
+import com.automobilepartnership.domain.member.persistence.Member;
+import com.automobilepartnership.domain.member.persistence.Role;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+@Getter
+public class UserPrincipal implements UserDetails, OAuth2User {
+
+    private final Long id;
+    private final String email;
+    private final String password;
+    private final String name;
+    private final int age;
+    private final String provider;
+    private final Role role;
+    private Map<String, Object> attributes;
+
+    public UserPrincipal(Member member) {
+        id = member.getId();
+        email = member.getEmail();
+        password = member.getPassword();
+        name = member.getName();
+        age = member.getAge();
+        provider = member.getProvider();
+        role = member.getRole();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<GrantedAuthority> authorities = new HashSet<>();
+        authorities.add(new SimpleGrantedAuthority(role.getKey()));
+
+        return authorities;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+}
