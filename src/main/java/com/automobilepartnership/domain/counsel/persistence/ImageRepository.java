06f715952bea21a70e476cf66e0da61b25ae5a10
yunhozz <qkrdbsgh1121@naver.com>
2022-10-16 10:57:50 +0900

    Feat: findByCounsel 쿼리 추가

--- src/main/java/com/automobilepartnership/domain/counsel/persistence/ImageRepository.java
+++ src/main/java/com/automobilepartnership/domain/counsel/persistence/ImageRepository.java
@@ -2,5 +2,9 @@ package com.automobilepartnership.domain.counsel.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ImageRepository extends JpaRepository<Image, Long> {
+
+    List<Image> findByCounsel(Counsel counsel);
 }
